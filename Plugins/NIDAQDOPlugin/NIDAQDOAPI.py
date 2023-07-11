bd76eb5d2db5f7e3edb410af4a8a493afc10f479
RyanX_CN <xy462819521@icloud.com>
2023-07-11 20:23:12 +0800

    QPSL_NIDAQDO.cpp DONE

--- Plugins/NIDAQDOPlugin/NIDAQDOAPI.py
+++ Plugins/NIDAQDOPlugin/NIDAQDOAPI.py
@@ -1,11 +1,26 @@
 from QPSLClass.Base import*
 
 class DAQmxDigitalOutputChannel(Structure):
-    pass
+    _fields_ = [('physical_line_name',c_char * 256)]
 
-c_DAQmxDigitalOutputChannel_P = POINTER(DAQmxDigitalOutputChannel)
+c_DAQmxDigitalOutputChannel_p = POINTER(DAQmxDigitalOutputChannel)
 
 class DAQmxDigitalOutputTask(Structure):
-    pass
+    _fields_ =[('handle', c_void_p),
+                ('channels', c_DAQmxDigitalOutputChannel_p),
+                ('line_number', c_uint32),
+                ('trigger_source', c_char * 1024), ('sample_rate', c_double),
+                ('sample_mode', c_int32), ('sample_per_channel', c_int32),
+                ('error_code', c_int32), ('error_buffer', c_char * 1024)]
+    #TODO
 
-c_DAQAnalogOutputTask_P = POINTER(DAQmxDigitalOutputTask)
+c_DAQAnalogOutputTask_p = POINTER(DAQmxDigitalOutputTask)
+
+c_int32_p = POINTER(c_int32)
+c_uint32_p = POINTER(c_uint32)
+c_double_p = POINTER(c_double)
+
+try:
+    _library = load_dll("QPSL_NIDAQDO.dll")
+except BaseException as e:
+    loading_error(e)
